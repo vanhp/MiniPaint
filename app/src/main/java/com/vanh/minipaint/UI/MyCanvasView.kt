4f867bb14528c70b2a715134e4409bcf9808a910
vanh <vanhphom@gmail.com>
2020-07-10 17:46:49 -0700

    prepare to handle touch event

--- app/src/main/java/com/vanh/minipaint/UI/MyCanvasView.kt
+++ app/src/main/java/com/vanh/minipaint/UI/MyCanvasView.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.Path
+import android.view.MotionEvent
 import android.view.View
 import androidx.core.content.res.ResourcesCompat
 import com.vanh.minipaint.R
@@ -16,6 +18,9 @@ class MyCanvasView (context:Context): View(context){
 
     private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
     private val drawColor = ResourcesCompat.getColor(resources, R.color.colorPaint, null)
+    private var path = Path() //drawing path
+    var motionTouchEventX = 0f
+    var motionTouchEventY = 0f
 
     // Set up the paint with which to draw.
     private val paint = Paint().apply {
@@ -45,5 +50,31 @@ class MyCanvasView (context:Context): View(context){
         super.onDraw(canvas)
 
         canvas.drawBitmap(extraBitmap,0f,0f,null)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        motionTouchEventX = event.x
+        motionTouchEventY = event.y
+
+        when (event.action) {
+               MotionEvent.ACTION_DOWN -> touchStart()
+               MotionEvent.ACTION_MOVE -> touchMove()
+               MotionEvent.ACTION_UP -> touchUp()
+        }
+        return true
+
+
+    }
+
+    private fun touchUp() {
+        TODO("Not yet implemented")
+    }
+
+    private fun touchMove() {
+        TODO("Not yet implemented")
+    }
+
+    private fun touchStart() {
+        TODO("Not yet implemented")
     }
 }
