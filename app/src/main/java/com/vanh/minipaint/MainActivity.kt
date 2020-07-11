8b49b63b72eb88f20ec6efbebe6b084873f3fecf
vanh <vanhphom@gmail.com>
2020-07-10 21:39:19 -0700

    fix when draw crash this is because it extrabitmap.recycle the bitmap before it use

--- app/src/main/java/com/vanh/minipaint/MainActivity.kt
+++ app/src/main/java/com/vanh/minipaint/MainActivity.kt
@@ -2,6 +2,7 @@ package com.vanh.minipaint
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
 import com.vanh.minipaint.UI.MyCanvasView
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +11,7 @@ class MainActivity : AppCompatActivity() {
 //         cannot get the size of the view in the onCreate() method,
 //         because the size has not been determined at this point. need screen size to draw
         val myCanvasView = MyCanvasView(this)
+        myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
         myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
         setContentView(myCanvasView)
     }
--- app/src/main/java/com/vanh/minipaint/UI/MyCanvasView.kt
+++ app/src/main/java/com/vanh/minipaint/UI/MyCanvasView.kt
@@ -31,26 +31,28 @@ class MyCanvasView (context:Context): View(context){
 
     // Set up the paint with which to draw.
     private val paint = Paint().apply {
-        color = Color.BLACK //drawColor
+        color = drawColor
         // Smooths out edges of what is drawn without affecting shape.
         isAntiAlias = true
         // Dithering affects how colors with higher-precision than the device are down-sampled.
         isDither = true
-        style = Paint.Style.STROKE // default: FILL
-        strokeJoin = Paint.Join.ROUND // default: MITER
-        strokeCap = Paint.Cap.ROUND // default: BUTT
-        strokeWidth = STROKE_WIDTH // default: Hairline-width (really thin)
+        style = Paint.Style.STROKE      // line default: FILL
+        strokeJoin = Paint.Join.ROUND   // line & curve join default: MITER
+        strokeCap = Paint.Cap.ROUND     // end of line of cap default: BUTT
+        strokeWidth = STROKE_WIDTH      // line width default: Hairline-width (really thin)
     }
 
 
     override fun onSizeChanged(width: Int, height: Int, oldwidth: Int, oldheight: Int) {
         super.onSizeChanged(width, height, oldwidth, oldheight)
 
+        // recyle old bitmap first when a new size is created
+        if (::extraBitmap.isInitialized) extraBitmap.recycle()
+
         extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888) // for caching
         extraCanvas = Canvas(extraBitmap)
         extraCanvas.drawColor(backgroundColor)
-        // recyle old bitmap when a new size is created
-        if (::extraBitmap.isInitialized) extraBitmap.recycle()
+
         // Calculate a rectangular frame around the picture.
         val inset = 40
         frame = Rect(inset, inset, width - inset, height - inset)
@@ -58,12 +60,12 @@ class MyCanvasView (context:Context): View(context){
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-//
-//        canvas.drawBitmap(extraBitmap,0f,0f,null)
+
+        canvas.drawBitmap(extraBitmap,0f,0f,null)
 //        canvas.drawRect(frame,paint)
-        canvas.drawPath(drawing,paint)
-        canvas.drawPath(curPath,paint)
-        canvas.drawRect(frame,paint)
+//        canvas.drawPath(drawing,paint)
+//        canvas.drawPath(curPath,paint)
+//        canvas.drawRect(frame,paint)
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
@@ -76,12 +78,9 @@ class MyCanvasView (context:Context): View(context){
                MotionEvent.ACTION_UP -> touchUp()
         }
         return true
-
-
     }
 
     private fun touchUp() {
-
         drawing.addPath(curPath)
         // Reset the path so it doesn't get drawn again.
         path.reset()
@@ -114,5 +113,5 @@ class MyCanvasView (context:Context): View(context){
            extraCanvas.drawPath(path, paint)
        }
        invalidate()
-}
+    }
 }
