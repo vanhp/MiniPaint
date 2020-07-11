218123c56d766205e0c5e33a1a71ca4f98960e8b
vanh <vanhphom@gmail.com>
2020-07-10 17:36:03 -0700

    setup painting structure

--- app/src/main/java/com/vanh/minipaint/UI/MyCanvasView.kt
+++ app/src/main/java/com/vanh/minipaint/UI/MyCanvasView.kt
@@ -3,15 +3,33 @@ package com.vanh.minipaint.UI
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Paint
 import android.view.View
 import androidx.core.content.res.ResourcesCompat
 import com.vanh.minipaint.R
+
+private const val STROKE_WIDTH = 12f  //must be float
 
 class MyCanvasView (context:Context): View(context){
     private lateinit var extraCanvas: Canvas
     private lateinit var extraBitmap: Bitmap
 
     private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+    private val drawColor = ResourcesCompat.getColor(resources, R.color.colorPaint, null)
+
+    // Set up the paint with which to draw.
+    private val paint = Paint().apply {
+        color = drawColor
+        // Smooths out edges of what is drawn without affecting shape.
+        isAntiAlias = true
+        // Dithering affects how colors with higher-precision than the device are down-sampled.
+        isDither = true
+        style = Paint.Style.STROKE // default: FILL
+        strokeJoin = Paint.Join.ROUND // default: MITER
+        strokeCap = Paint.Cap.ROUND // default: BUTT
+        strokeWidth = STROKE_WIDTH // default: Hairline-width (really thin)
+    }
+
 
     override fun onSizeChanged(width: Int, height: Int, oldwidth: Int, oldheight: Int) {
         super.onSizeChanged(width, height, oldwidth, oldheight)
